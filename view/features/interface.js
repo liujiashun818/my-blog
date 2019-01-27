ab02f54d06cb3cade37d97f7a67cf2ebb9b0a490
liujiashun818 <1696054400@qq.com>
2019-01-27 18:09:21 +0800

    'dddd'

--- view/features/interface.js
+++ view/features/interface.js
@@ -7,10 +7,10 @@ const {
 } = switchDomain.getBackEndIp();
 
 // export const queryUrl = "../dist/a.json";
-// export const queryArticlesUrl = 'http://127.0.0.1:7001' + '/api/articles';
-export const queryArticlesUrl = 'localhost:7001' + '/api/articles';
-// export const regirstUrl = 'http://127.0.0.1:7001' + '/api/users/signup';
-export const regirstUrl = 'localhost:7001' + '/api/users/signup';
-// export const signInUrl = 'http://127.0.0.1:7001' + '/api/users/signin';
-export const signInUrl = 'localhost:7001' + '/api/users/signin';
+export const queryArticlesUrl = 'http://127.0.0.1:7001' + '/api/articles';
+// export const queryArticlesUrl = 'localhost:7001' + '/api/articles';
+export const regirstUrl = 'http://127.0.0.1:7001' + '/api/users/signup';
+// export const regirstUrl = 'localhost:7001' + '/api/users/signup';
+export const signInUrl = 'http://127.0.0.1:7001' + '/api/users/signin';
+// export const signInUrl = 'localhost:7001' + '/api/users/signin';
 
