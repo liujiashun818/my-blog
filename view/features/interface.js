a5479bf21858565328d75b1cd51706d10c55402b
liujiashun818 <1696054400@qq.com>
2019-01-27 15:09:32 +0800

    'url'

--- view/features/interface.js
+++ view/features/interface.js
@@ -7,6 +7,10 @@ const {
 } = switchDomain.getBackEndIp();
 
 // export const queryUrl = "../dist/a.json";
-export const queryArticlesUrl = 'http://127.0.0.1:7001' + '/api/articles';
-export const regirstUrl = 'http://127.0.0.1:7001' + '/api/users/signup';
-export const signInUrl = 'http://127.0.0.1:7001' + '/api/users/signin';
+// export const queryArticlesUrl = 'http://127.0.0.1:7001' + '/api/articles';
+export const queryArticlesUrl = 'http://39.107.73.145:7001' + '/api/articles';
+// export const regirstUrl = 'http://127.0.0.1:7001' + '/api/users/signup';
+export const regirstUrl = 'http://39.107.73.145:7001' + '/api/users/signup';
+// export const signInUrl = 'http://127.0.0.1:7001' + '/api/users/signin';
+export const signInUrl = 'http://39.107.73.145:7001' + '/api/users/signin';
+
