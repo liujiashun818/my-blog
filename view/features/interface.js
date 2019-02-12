2222f1607e3c3c58052f108c716a7f5cb6c571db
liujiashun818 <1696054400@qq.com>
2019-02-12 23:55:46 +0800

    feat:记得修改dist生成目录

--- view/features/interface.js
+++ view/features/interface.js
@@ -5,12 +5,14 @@ import switchDomain from './switchDoman.js';
 const {
   baseOrigin,
 } = switchDomain.getBackEndIp();
+const ipHost = 'http://39.107.73.145:7001';
+const baseHost = 'http://127.0.0.1:7001';
 
 // export const queryUrl = "../dist/a.json";
-export const queryArticlesUrl = 'http://127.0.0.1:7001' + '/api/articles';
+export const queryArticlesUrl = ipHost + '/api/articles';
 // export const queryArticlesUrl = 'localhost:7001' + '/api/articles';
-export const regirstUrl = 'http://127.0.0.1:7001' + '/api/users/signup';
+export const regirstUrl = ipHost + '/api/users/signup';
 // export const regirstUrl = 'localhost:7001' + '/api/users/signup';
-export const signInUrl = 'http://127.0.0.1:7001' + '/api/users/signin';
+export const signInUrl = ipHost + '/api/users/signin';
 // export const signInUrl = 'localhost:7001' + '/api/users/signin';
 
