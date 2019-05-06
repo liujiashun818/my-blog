f254119f9397c94526c2157fcbf021b894a6e333
liujiashun <1696054400@qq.com>
2019-05-06 18:50:32 +0800

    Update interface.js

--- view/features/interface.js
+++ view/features/interface.js
@@ -10,13 +10,14 @@ const baseHost = 'http://127.0.0.1:7001';
 
 // export const queryUrl = "../dist/a.json";
 export const queryArticlesUrl = `${ipHost}/api/articles`;
-// export const queryArticlesUrl = 'localhost:7001' + '/api/articles';
+// export const queryArticlesUrl = `${baseHost}/api/articles`;
 export const regirstUrl = `${ipHost}/api/users/signup`;
-// export const regirstUrl = 'localhost:7001' + '/api/users/signup';
+// export const regirstUrl = `${baseHost}/api/users/signup`;
 export const signInUrl = `${ipHost}/api/users/signin`;
-// export const signInUrl = 'localhost:7001' + '/api/users/signin';
+// export const signInUrl = `${baseHost}/api/users/signin`;
 // export const categoriesUrl = ipHost + '/api/categories'; //分类
 export const articleUrl = `${ipHost}/api/articles`; // 保存文章
-// export const articleUrl = `${ipHost}/api/articles`; // 编辑文章
+// export const articleUrl = `${baseHost}/api/articles`; 
 export const detailUrl = `${ipHost}/api/articles/detail`; // 获取文章详情
+// export const detailUrl = `${baseHost}/api/articles/detail`; // 获取文章详情
 
